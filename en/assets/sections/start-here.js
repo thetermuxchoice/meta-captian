0e7b411fa3aab9559ea29609406ecd3aad35da87
calvin <garrixa6@gmail.com>
2022-11-23 17:25:50 -0500

    start here page agregado

--- en/assets/sections/start-here.js
+++ en/assets/sections/start-here.js
@@ -1,48 +1,11 @@
+const btnControl = document.querySelectorAll(".btn-control"),
+	  creationItems = document.querySelectorAll(".creation-item")
 
-			(function() {
-
-				var overlay = document.getElementById( 'overlay' ),
-					overlayClose = overlay.querySelector( 'button' ),
-					header = document.getElementById( 'header' )
-					switchBtnn = header.querySelector( 'button.slider-switch' ),
-					toggleBtnn = function() {
-						if( slideshow.isFullscreen ) {
-							classie.add( switchBtnn, 'view-maxi' );
-						}
-						else {
-							classie.remove( switchBtnn, 'view-maxi' );
-						}
-					},
-					toggleCtrls = function() {
-						if( !slideshow.isContent ) {
-							classie.add( header, 'hide' );
-						}
-					},
-					toggleCompleteCtrls = function() {
-						if( !slideshow.isContent ) {
-							classie.remove( header, 'hide' );
-						}
-					},
-					slideshow = new DragSlideshow( document.getElementById( 'slideshow' ), { 
-						// toggle between fullscreen and minimized slideshow
-						onToggle : toggleBtnn,
-						// toggle the main image and the content view
-						onToggleContent : toggleCtrls,
-						// toggle the main image and the content view (triggered after the animation ends)
-						onToggleContentComplete : toggleCompleteCtrls
-					}),
-					toggleSlideshow = function() {
-						slideshow.toggle();
-						toggleBtnn();
-					},
-					closeOverlay = function() {
-						classie.add( overlay, 'hide' );
-					};
-
-				// toggle between fullscreen and small slideshow
-				switchBtnn.addEventListener( 'click', toggleSlideshow );
-				// close overlay
-				overlayClose.addEventListener( 'click', closeOverlay );
-
-			}());
-		
+for (let index = 0; index < btnControl.length; index++) {
+	btnControl[index].addEventListener("click", ()=>{
+		if (document.querySelector(".creation-item-open")) {
+			document.querySelector(".creation-item-open").classList.remove("creation-item-open")	
+		}
+		creationItems[index].classList.add("creation-item-open")
+	})
+}
