cd4b1125f455fe3897a93c0ee81f6b6efcb670f6
calvin <garrixa6@gmail.com>
2022-11-25 13:42:02 -0500

    about & contact add

--- en/assets/sections/start-here.js
+++ en/assets/sections/start-here.js
@@ -1,11 +1,17 @@
-const btnControl = document.querySelectorAll(".btn-control"),
-	  creationItems = document.querySelectorAll(".creation-item")
+const btnNext = document.querySelector("#next-slider")
+const sliders = document.querySelectorAll(".start-container--slider")
 
-for (let index = 0; index < btnControl.length; index++) {
-	btnControl[index].addEventListener("click", ()=>{
-		if (document.querySelector(".creation-item-open")) {
-			document.querySelector(".creation-item-open").classList.remove("creation-item-open")	
-		}
-		creationItems[index].classList.add("creation-item-open")
-	})
-}
+let contadorSliders = 1
+
+btnNext.addEventListener("click",()=>{
+	if (contadorSliders == 1) {
+		sliders[contadorSliders-1].classList.add("clip-0")
+	}
+	if (contadorSliders == 8) {
+		contadorSliders = 1
+		return
+	}
+	sliders[contadorSliders-1].classList.remove("start-container--slider-active")
+	sliders[contadorSliders].classList.add("start-container--slider-active")
+	contadorSliders += 1
+})
